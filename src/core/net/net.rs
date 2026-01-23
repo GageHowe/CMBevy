90916f705c0769ada32f4e92b712b47bf61179a6
Gage Howe <howe.gaged@gmail.com>
2026-01-22 18:19:34 -0600

    started networking client and server

--- src/core/net/net.rs
+++ src/core/net/net.rs
@@ -1,3 +1,22 @@
 // use bevy_renet::*;
 
 // this module will contain information about packets, synchronization, etc
+use bevy::prelude::*;
+use bevy_renet::renet::*;
+use bevy_renet::*;
+use serde::{Deserialize, Serialize};
+
+// makes sure clients are on the same version
+pub const VERSION: u64 = 1000;
+
+// https://www.youtube.com/watch?v=fBHO0yptg1Y
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Ping {
+    PingPing,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Pong {
+    PongPong,
+}
