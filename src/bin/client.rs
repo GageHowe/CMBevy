f74a8bdf6e356671596a50db5f8e82826ebec239
Gage Howe <howe.gaged@gmail.com>
2026-01-23 01:41:02 -0600

    added some net stuff, compression

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,19 +1,108 @@
 use bevy::prelude::*;
+// use bevy_renet::renet::*;
+use bevy_renet::netcode::*;
+use bevy_renet::renet::{ConnectionConfig, DefaultChannel};
+use bevy_renet::*;
 use cmbevy::core::level::level::*;
 use cmbevy::core::physics::physics_world::*;
 use cmbevy::core::player::player::*;
 use cmbevy::core::ui::ui::UIPlugin;
 use cmbevy::core::window::*;
+use std::net::UdpSocket;
+use std::time::SystemTime;
+
+// use ruzstd::decoding::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        // .add_plugins(FrameTimeDiagnosticsPlugin::default())
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
         .insert_resource(Time::<Fixed>::from_hz(60.0))
         .add_plugins(WindowSettingsPlugin)
         .add_plugins(PhysicsPlugin)
         .add_plugins(PlayerPlugin)
         .add_plugins(LevelPlugin)
-        .add_plugins(UIPlugin)
-        .run();
+        .add_plugins(UIPlugin);
+
+    // renet setup
+    app.add_plugins(RenetClientPlugin);
+
+    let client = RenetClient::new(ConnectionConfig::default());
+    app.insert_resource(client);
+
+    // Setup the transport layer
+    app.add_plugins(NetcodeClientPlugin);
+
+    let authentication = ClientAuthentication::Unsecure {
+        server_addr: "127.0.0.1:5000".parse().unwrap(),
+        client_id: 0,
+        user_data: None,
+        protocol_id: 0,
+    };
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let current_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+    let mut transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+    app.insert_resource(transport);
+
+    app.add_systems(Startup, send_message_system);
+    app.add_systems(Startup, receive_message_system);
+    // :)
+    app.run();
 }
+
+// use ruzstd here
+
+fn send_message_system(mut client: ResMut<RenetClient>) {
+    // Send a text message to the server
+    client.send_message(DefaultChannel::ReliableOrdered, "server message");
+}
+
+fn receive_message_system(mut client: ResMut<RenetClient>) {
+    while let Some(message) = client.receive_message(DefaultChannel::ReliableOrdered) {
+        // Handle received message
+    }
+}
+/*
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugin(RenetClientPlugin);
+
+    let client = RenetClient::new(ConnectionConfig::default());
+    app.insert_resource(client);
+
+    // Setup the transport layer
+    app.add_plugin(NetcodeClientPlugin);
+
+    let authentication = ClientAuthentication::Unsecure {
+        server_addr: SERVER_ADDR,
+        client_id: 0,
+        user_data: None,
+        protocol_id: 0,
+    };
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    let mut transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+    app.insert_resource(transport);
+
+    app.add_system(send_message_system);
+    app.add_system(receive_message_system);
+}
+
+// Systems
+
+fn send_message_system(mut client: ResMut<RenetClient>) {
+    // Send a text message to the server
+    client.send_message(DefaultChannel::ReliableOrdered, "server message");
+}
+
+fn receive_message_system(mut client: ResMut<RenetClient>) {
+    while let Some(message) = client.receive_message(DefaultChannel::ReliableOrdered) {
+        // Handle received message
+    }
+}
+
+    */
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,25 +4,154 @@ use bevy::render::settings::{RenderCreation, WgpuSettings};
 use cmbevy::core::level::level::*;
 use cmbevy::core::physics::physics_world::*;
 use cmbevy::core::player::player::*;
+
+use bevy_renet::netcode::*;
+use bevy_renet::renet::ServerEvent;
+use bevy_renet::renet::{ConnectionConfig, DefaultChannel};
+use bevy_renet::*;
+use std::net::UdpSocket;
+use std::time::SystemTime;
+// use bevy_renet::renet::
+
 fn main() {
-    App::new()
-        // dirty workaround from https://taintedcoders.com/bevy/how-to/headless-mode
-        .add_plugins(
-            DefaultPlugins
-                // .set(ScheduleRunnerPlugin::run_once())
-                .set(RenderPlugin {
-                    // synchronous_pipeline_compilation: true,
-                    render_creation: RenderCreation::Automatic(WgpuSettings {
-                        backends: None,
-                        ..default()
-                    }),
+    let mut app = App::new();
+    app.add_plugins(
+        // https://taintedcoders.com/bevy/how-to/headless-mode
+        DefaultPlugins
+            // .set(ScheduleRunnerPlugin::run_once())
+            .set(RenderPlugin {
+                // synchronous_pipeline_compilation: true,
+                render_creation: RenderCreation::Automatic(WgpuSettings {
+                    backends: None,
                     ..default()
                 }),
-        )
-        // .add_plugins(FrameTimeDiagnosticsPlugin::default())
-        .insert_resource(Time::<Fixed>::from_hz(60.0))
-        .add_plugins(PhysicsPlugin)
-        .add_plugins(PlayerPlugin)
-        .add_plugins(LevelPlugin)
-        .run();
+                ..default()
+            }),
+    )
+    // .add_plugins(FrameTimeDiagnosticsPlugin::default())
+    .insert_resource(Time::<Fixed>::from_hz(60.0))
+    .add_plugins(PhysicsPlugin)
+    .add_plugins(PlayerPlugin)
+    .add_plugins(LevelPlugin);
+
+    // renet setup
+    app.add_plugins(RenetServerPlugin);
+
+    let server = RenetServer::new(ConnectionConfig::default());
+    app.insert_resource(server);
+
+    // Transport layer setup
+    app.add_plugins(NetcodeServerPlugin);
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    let server_config = ServerConfig {
+        current_time: SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap(),
+        max_clients: 64,
+        protocol_id: 0,
+        public_addresses: vec![server_addr],
+        authentication: ServerAuthentication::Unsecure,
+    };
+    let transport = NetcodeServerTransport::new(server_config, socket).unwrap();
+    app.insert_resource(transport);
+
+    app.add_systems(Startup, send_message_system);
+    app.add_systems(Startup, receive_message_system);
+    app.add_systems(Startup, handle_events_system);
+
+    // start server
+    app.run();
 }
+
+fn send_message_system(mut server: ResMut<RenetServer>) {
+    let channel_id = 0;
+    // Send a text message for all clients
+    // The enum DefaultChannel describe the channels used by the default configuration
+    server.broadcast_message(DefaultChannel::ReliableOrdered, "server message");
+}
+
+fn receive_message_system(mut server: ResMut<RenetServer>) {
+    // Receive message from all clients
+    for client_id in server.clients_id() {
+        while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered)
+        {
+            // Handle received message
+        }
+    }
+}
+
+fn handle_events_system(
+    mut server_events: MessageReader<ServerEvent>,
+    mut server: ResMut<RenetServer>,
+) {
+    for event in server_events.read() {
+        match event {
+            ServerEvent::ClientConnected { client_id } => {
+                println!("Client {client_id} connected");
+            }
+            ServerEvent::ClientDisconnected { client_id, reason } => {
+                println!("Client {client_id} disconnected: {reason}");
+            }
+        }
+    }
+}
+
+/*
+fn main() {
+    let mut app = App::new();
+    app.add_plugin(RenetServerPlugin);
+
+    let server = RenetServer::new(ConnectionConfig::default());
+    app.insert_resource(server);
+
+    // Transport layer setup
+    app.add_plugin(NetcodeServerPlugin);
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    let server_config = ServerConfig {
+        current_time: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(),
+        max_clients: 64,
+        protocol_id: 0,
+        public_addresses: vec![server_addr],
+        authentication: ServerAuthentication::Unsecure
+    };
+    let transport = NetcodeServerTransport::new(server_config, socket).unwrap();
+    app.insert_resource(transport);
+
+    app.add_system(send_message_system);
+    app.add_system(receive_message_system);
+    app.add_system(handle_events_system);
+}
+
+// Systems
+
+fn send_message_system(mut server: ResMut<RenetServer>) {
+    let channel_id = 0;
+    // Send a text message for all clients
+    // The enum DefaultChannel describe the channels used by the default configuration
+    server.broadcast_message(DefaultChannel::ReliableOrdered, "server message");
+}
+
+fn receive_message_system(mut server: ResMut<RenetServer>) {
+    // Receive message from all clients
+    for client_id in server.clients_id() {
+        while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered) {
+            // Handle received message
+        }
+    }
+}
+
+fn handle_events_system(mut server_events: MessageReader<ServerEvent>) {
+    for event in server_events.read() {
+        match event {
+            ServerEvent::ClientConnected { client_id } => {
+                println!("Client {client_id} connected");
+            }
+            ServerEvent::ClientDisconnected { client_id, reason } => {
+                println!("Client {client_id} disconnected: {reason}");
+            }
+        }
+    }
+}
+     */
