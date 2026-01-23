9fd86d423170b59b4bd1361910db83d8fa260661
Gage Howe <howe.gaged@gmail.com>
2026-01-23 04:09:18 -0600

    kjldsf

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -82,7 +82,7 @@ fn receive_message_system(mut server: ResMut<RenetServer>) {
 }
 
 fn handle_events_system(
-    mut server_events: MessageReader<ServerEvent>,
+    mut server_events: MessageReader<ServerEvent>, // maybe EventReader etc?
     mut server: ResMut<RenetServer>,
 ) {
     for event in server_events.read() {
@@ -96,62 +96,3 @@ fn handle_events_system(
         }
     }
 }
-
-/*
-fn main() {
-    let mut app = App::new();
-    app.add_plugin(RenetServerPlugin);
-
-    let server = RenetServer::new(ConnectionConfig::default());
-    app.insert_resource(server);
-
-    // Transport layer setup
-    app.add_plugin(NetcodeServerPlugin);
-    let server_addr = "127.0.0.1:5000".parse().unwrap();
-    let socket = UdpSocket::bind(server_addr).unwrap();
-    let server_config = ServerConfig {
-        current_time: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(),
-        max_clients: 64,
-        protocol_id: 0,
-        public_addresses: vec![server_addr],
-        authentication: ServerAuthentication::Unsecure
-    };
-    let transport = NetcodeServerTransport::new(server_config, socket).unwrap();
-    app.insert_resource(transport);
-
-    app.add_system(send_message_system);
-    app.add_system(receive_message_system);
-    app.add_system(handle_events_system);
-}
-
-// Systems
-
-fn send_message_system(mut server: ResMut<RenetServer>) {
-    let channel_id = 0;
-    // Send a text message for all clients
-    // The enum DefaultChannel describe the channels used by the default configuration
-    server.broadcast_message(DefaultChannel::ReliableOrdered, "server message");
-}
-
-fn receive_message_system(mut server: ResMut<RenetServer>) {
-    // Receive message from all clients
-    for client_id in server.clients_id() {
-        while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered) {
-            // Handle received message
-        }
-    }
-}
-
-fn handle_events_system(mut server_events: MessageReader<ServerEvent>) {
-    for event in server_events.read() {
-        match event {
-            ServerEvent::ClientConnected { client_id } => {
-                println!("Client {client_id} connected");
-            }
-            ServerEvent::ClientDisconnected { client_id, reason } => {
-                println!("Client {client_id} disconnected: {reason}");
-            }
-        }
-    }
-}
-     */
