3119e06da071b2fa1241df4463c5e282a118a36d
Gage Howe <howe.gaged@gmail.com>
2026-01-24 16:43:16 -0600

    kljsdfls

--- src/bin/client/client.rs
+++ src/bin/client/client.rs
@@ -1,9 +1,8 @@
 // client executable
 
 mod net;
-
+use bevy::log::{Level, LogPlugin};
 use bevy::prelude::*;
-use cmbevy::core::config::SERVER_ADDRESS;
 use cmbevy::core::{
     level::level::*,
     physics::{components::*, physics_world::*},
@@ -28,18 +27,22 @@ enum AppState {
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins)
-        .insert_resource(Time::<Fixed>::from_hz(60.0))
-        .init_state::<AppState>()
-        .add_plugins(AppSettingsPlugin)
-        .add_plugins(WindowSettingsPlugin)
-        .add_plugins(PhysicsPlugin)
-        .add_plugins(PlayerPlugin)
-        .add_plugins(LevelPlugin)
-        .add_plugins(UIPlugin)
-        .add_plugins(ClientNetManagerPlugin);
+    app.add_plugins(DefaultPlugins.set(LogPlugin {
+        level: Level::WARN,
+        ..default()
+    }))
+    .insert_resource(Time::<Fixed>::from_hz(60.0))
+    .init_state::<AppState>()
+    .add_plugins(AppSettingsPlugin)
+    .add_plugins(WindowSettingsPlugin)
+    .add_plugins(PhysicsPlugin)
+    .add_plugins(PlayerPlugin)
+    .add_plugins(LevelPlugin)
+    .add_plugins(UIPlugin)
+    .add_plugins(ClientNetManagerPlugin);
 
     // :)
+    println!("starting server...\n");
     app.run();
 }
 
